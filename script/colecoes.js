5cca2dd3c9134d7b0a82f3f61103443842c701f9
Matheus Soldesi <matheus.soldesi@gmail.com>
2026-05-26 19:30:14 -0300

    Create colecoes.js

---
+++ script/colecoes.js
@@ -0,0 +1,9 @@
+use leads_db
+
+db.createCollection("users")
+db.createCollection("teams")
+db.createCollection("stores")
+db.createCollection("customers")
+db.createCollection("leads")
+db.createCollection("negotiations")
+db.createCollection("logs")
